bef8fb94cd74639b98a23ad8e6be0c75ef00633f
Erwin Korsten <mw.korsten@gmail.com>
2019-02-13 10:40:10 +0100

    Added the time to the page. Created an Angular filter for time display

--- js/app.js
+++ js/app.js
@@ -24,3 +24,28 @@ runnersApp.filter(`split`, function() {
 		return input.split(splitChar)[splitIndex];
 	}
 });
+
+runnersApp.filter(`time`, function() {
+	return function(input) {
+		let time = new Date(null);
+		time.setSeconds(input);
+		return time.toISOString().substr(11, 8);
+	}
+});
+
+runnersApp.filter(`speedruntime`, function() {
+	return function(input) {
+		let time = Math.floor(input);
+		let secondsDec = Math.round((input - time)*1000);
+		let hour = Math.floor(time / 3600);
+		hour = (`0${hour}`).slice(-2);
+		let minutes = Math.floor(time % 3600 / 60);
+		minutes = (`0${minutes}`).slice(-2);
+		let seconds = time % 3600 % 60;
+		seconds = (`0${seconds}`).slice(-2);
+		if (secondsDec !== 0){
+		seconds = `${seconds}.${secondsDec}`;
+		} 
+		return `${hour}:${minutes}:${seconds}`;
+	}
+});
